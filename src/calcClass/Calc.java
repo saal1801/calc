4bff23e3e8b8074b649af1cf0fafc1fc84f08e74
Sarko <sarko@Sarkos-MacBook-Pro.local>
2020-01-24 11:34:17 +0100

    firstcalc

---
+++ src/calcClass/Calc.java
@@ -0,0 +1,47 @@
+package calcClass;
+
+import java.util.Scanner;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Following operations are supported : \n" +
+				  "1. Addition (+) \n" +
+				  "2. Subtraction (-) \n" +
+				  "3. Multiplication (*) \n" +
+				  "4. Division (/) \n");
+		
+		Scanner scanner = new Scanner (System.in);
+		char operation = scanner.next().charAt(0);
+		
+		System.out.print("Enter the first number: ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("Enter the second number: ");
+		int num2 = scanner.nextInt();
+		
+		if (!(operation == '+' || operation == '-' ||operation == '*' ||operation == '/')) {
+			System.err.println("Invalid Operator. Please use only + or - or * or /");
+		}
+		
+		/*if(!(operation == '/' && num2 == 0.0)) {
+			System.err.println("The second number cannot be zero for division operation.");
+		}*/
+		
+		if (operation == '+') {
+			System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		}
+		if (operation == '-') {
+			System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		}
+		if (operation == '*') {
+			System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+		}
+		if (operation == '/') {
+			System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+		}
+
+	}
+
+}
